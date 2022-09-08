b85ee2935877a1946a207debe6e4c97598a06287
Tella Adesina <telladesina100@gmail.com>
2022-09-08 18:36:49 -0500

    Added-Swagger

---
+++ Bio_Core/Models/BioConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bio_Core.Models
+{
+    public class BioConfig
+    {
+        public string baseUrl { get; set; }
+    }
+}
